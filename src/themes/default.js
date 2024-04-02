346637227fd9f4917cd984248a8908df105d1498
prajwalsoni <prajwalsonics@gmail.com>
2024-04-02 12:32:16 +0530

    updated

--- src/themes/default.js
+++ src/themes/default.js
@@ -16,11 +16,12 @@ export default {
     text3: "#575C66",
     footerBackground: "#00012B"
   },
-  // Breakpoints for responsive design
+  // Adjusted breakpoints for responsive design
   breakpoints: {
-    sm: 'screen and (max-width: 640px)',
-    md: 'screen and (max-width: 768px)',
-    lg: 'screen and (max-width: 1024px)',
-    xl: 'screen and (max-width: 1280px)'
+    xs: 'screen and (max-width: 480px)', // Extra small devices (portrait phones)
+    sm: 'screen and (max-width: 768px)', // Small devices (landscape phones)
+    md: 'screen and (max-width: 1024px)', // Medium devices (tablets)
+    lg: 'screen and (max-width: 1280px)', // Large devices (desktops)
+    xl: 'screen and (max-width: 1536px)' // Extra large devices (large desktops)
   },
 }
